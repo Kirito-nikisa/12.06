c599f815c6d37ae8ede07665a9bf2ef8f125510f
Kirito-nikisa <nikosa090@gmail.com>
2024-06-12 16:42:12 +0300

    Added hom1 ex1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
+int linearSearch(int arr[], int size, int key) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == key) {
+            return i;
+        }
+    }
+    return -1;
+}
 
+int main() {
+    int arr[] = { 5, 3, 7, 2, 8, 1, 9 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int key = 7;
+
+    int result = linearSearch(arr, size, key);
+
+    if (result != -1) {
+        cout << "Element found at index: " << result << endl;
+    }
+    else {
+        cout << "Element not found in array." << endl;
+    }
+
+    return 0;
 }
