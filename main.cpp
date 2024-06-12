eefb021d45d22b3f996998920c0ee6ca53c7e66c
Kirito-nikisa <nikosa090@gmail.com>
2024-06-12 16:45:24 +0300

    Added hom1 ex3

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-int main() {
+int binaryToDecimal(string binary) {
+    int decimal = 0;
+    int base = 1;
 
+    for (int i = binary.length() - 1; i >= 0; i--) {
+        if (binary[i] == '1') {
+            decimal += base;
+        }
+        base *= 2;
+    }
+
+    return decimal;
+}
+
+int main() {
+    string binaryNumber = "1101"; 
+
+    int decimalNumber = binaryToDecimal(binaryNumber);
+
+    cout << "The decimal representation of " << binaryNumber << " is " << decimalNumber << endl;
+
+    return 0;
 }
